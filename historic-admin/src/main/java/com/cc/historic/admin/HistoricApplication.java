0e0bcd4bc3713c7ec8dbb2d66fd7e0f9df91dc2f
carlosxiao <carlosxiao@dr.com>
2019-04-10 11:59:16 +0800

    add default welcome request mapping.

--- historic-admin/src/main/java/com/cc/historic/admin/HistoricApplication.java
+++ historic-admin/src/main/java/com/cc/historic/admin/HistoricApplication.java
@@ -2,7 +2,11 @@ package com.cc.historic.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -12,9 +16,21 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.cc.historic.admin.model.mapper")
 @EnableTransactionManagement
 public class HistoricApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HistoricApplication.class);
 
-	}
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("forward:/admin/index");
+                registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoricApplication.class);
+
+    }
 
 }
