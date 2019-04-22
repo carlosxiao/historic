ee0dd74242cffe4678f932adbf001f61128391e1
carlosxiao <carlosxiao@dr.com>
2019-04-22 10:44:50 +0800

    添加支付文档以及支付宝接入

---
+++ historic-web/src/main/java/com/cc/historic/web/alipay/AlipayConfig.java
@@ -0,0 +1,72 @@
+﻿package com.cc.historic.web.alipay;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/* *
+ *类名：AlipayConfig
+ *功能：基础配置类
+ *详细：设置帐户有关信息及返回路径
+ *修改日期：2017-04-05
+ *说明：
+ *以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
+ *该代码仅供学习和研究支付宝接口使用，只是提供一个参考。
+ */
+
+public class AlipayConfig {
+	
+    //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+	// 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+	public static String app_id = "2016092600597664";
+	
+	// 商户私钥，您的PKCS8格式RSA2私钥
+    public static String merchant_private_key = "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDKcli8steu1iHQvuIyqWI/KJB2Eff6nj7IdsuV1AW8bDw0apMpD2GzpJ9YLa5LhQgIkTEwdlUj5E5XOiiz6wPlNQZIM/GTuBylzlb2lnXKMhMbT5Pq24j0QcKMmoUuOV/Hvlow6nvKE4ljLuSbUFBQ4j/H7CwoH+d8Bu76yqdyp4JY6gNs28tJK9zhn9jz2Tu0C7W5fv49mHsgZtrd28HC4ncL35Ymgzdzq/FpdOeLChHqMF1MyEavkKkoZyep2K16ECcVvMWGmMjWkCl8X1hyOccKFgJjE3FiDrYwWZVjaBWrJIst34OgqNR+KWjZRC0ec4vApIeeA0MvqAVuHWYLAgMBAAECggEBAJBN/7jKFnnWwxXj6qLIIHL6vDadSlEHmaMVIrsy/b+4uxV13l4HS9JchwS40yObuQU6UYDE6J2D8N+2YisVW5p3OHohJJLySuFZep2c1GuDVCFEkSB4yl+pvD6zKjmvxflAU1PI4OJDGMxtMaEQF6AAFgyw6SzUyRGcc56q878CKYTKVaVbjRyyjJ3ea4UHH/ORuXHZXTHDsh9s3GHKw2z4tJtyW4dErsgiI23bDtlCn/dC9eGIDVTlEexaz0Y3W7nvAx/GHi0cxA6X0Oy1uHTpL5PB0NcnRbH15H4YzseSOBCfxrf82B/qb8g4JfjGMsccOq+ssuBPquZF42HBKdECgYEA6JV4TIQTIRwoT/FI7SR2KY3oVug3DS9rYGSPPnRRXGMntZQDqTq3qb7+TajQh5bYgDc2EO+KHS/jrWK46OLb+BCEew/GuyEQSeGMXE1hwiVLub91UjTWl86NP20cbFpCZfkyZURUOgmtX1ukM9AQq6ZmgRLKaqAQMLhtNJhFubkCgYEA3tQhuFBbAbDQ7gEkIUgodN45s5L5WiaeJbRCCM3JQZ4+vUI55E76KTa98I+OAcWulBsBdb3GKTVlppwtIc9GZMAAB4CzoNgmBMnjPHxZRzhX5WyvGPFTkiuGNQeW+/oOIS6ctmDbnmUKIn9sfyMvxVOaaVB98QO+kRUVWyoW7+MCgYEA1nLy38IrjSzUMpmr/xPOa5QvtOoZ0Gt3mwI9XX2Mgx3dwDuAgKnmLHoxKlPMKii46Md/b3AeLbk5OWY8TmbWIUzBimJKf8IFrclsTLMAvyz6PIM+3pRfhMtrSx8ExRcX/8QcZmjxLC4+EB2EMk3XAR3Fh59WyadaJkH2VzdLmukCgYBWJ1i+RbtaJP4y95f7kkWqIf3XU307gPDmjRJ3CvHEgCWr0T5G6s5IPxUjPK3gO4lrVCG1X2GNr0qVJ48/oj+KVr3sKLPK/ad9dH16dm2iXsOFPgdZMn1oZ5GlUUDsPdtWswn+beacEF7hxbiyU2118wQhsfBvzk3RFIDEVbq2lwKBgQCafY/dmqjDVYZ5QF90u4HkE6UxgNTGtHe1rM9LUEhCFNWGCDorDQAFaCS1ehvp3NapEVH/FW9/qw5ZT/YYYZ5F8nXy29Hl3uXNgTdFsLlh5g+dnkdCuvchFuMAGywFngVNof7mcmb10kIOCNTPtA15phwY7411MPkdcyax39qptw==";
+	
+	// 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAynJYvLLXrtYh0L7iMqliPyiQdhH3+p4+yHbLldQFvGw8NGqTKQ9hs6SfWC2uS4UICJExMHZVI+ROVzoos+sD5TUGSDPxk7gcpc5W9pZ1yjITG0+T6tuI9EHCjJqFLjlfx75aMOp7yhOJYy7km1BQUOI/x+wsKB/nfAbu+sqncqeCWOoDbNvLSSvc4Z/Y89k7tAu1uX7+PZh7IGba3dvBwuJ3C9+WJoM3c6vxaXTniwoR6jBdTMhGr5CpKGcnqditehAnFbzFhpjI1pApfF9YcjnHChYCYxNxYg62MFmVY2gVqySLLd+DoKjUfilo2UQtHnOLwKSHngNDL6gFbh1mCwIDAQAB";
+
+	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+	public static String notify_url = "http://工程公网访问地址/alipay.trade.page.pay-JAVA-UTF-8/notify_url.jsp";
+
+	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+	public static String return_url = "http://工程公网访问地址/alipay.trade.page.pay-JAVA-UTF-8/return_url.jsp";
+
+	// 签名方式
+	public static String sign_type = "RSA2";
+	
+	// 字符编码格式
+	public static String charset = "utf-8";
+	
+	// 支付宝网关
+	public static String gatewayUrl = "https://openapi.alipay.com/gateway.do";
+	
+	// 支付宝网关
+	public static String log_path = "C:\\";
+
+
+//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+    /** 
+     * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
+     * @param sWord 要写入日志里的文本内容
+     */
+    public static void logResult(String sWord) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(log_path + "alipay_log_" + System.currentTimeMillis()+".txt");
+            writer.write(sWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
--- historic-web/src/main/java/com/cc/historic/web/controller/GoodsController.java
+++ historic-web/src/main/java/com/cc/historic/web/controller/GoodsController.java
@@ -1,8 +1,21 @@
 package com.cc.historic.web.controller;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.cc.historic.web.alipay.AlipayConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.sun.xml.internal.ws.api.message.Packet.State.ServerResponse;
 
 /**
  * @author xiao
@@ -10,10 +23,112 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("goods")
+@Slf4j
 public class GoodsController {
 
     @RequestMapping("{id}")
     public String good(@PathVariable() Integer id) {
         return "goods_details";
+    }
+
+    /**
+     * 生成订单
+     * @return
+     */
+    @PostMapping("createOrder")
+    public String createOrder() {
+
+        String orderId = "";
+        return orderId;
+    }
+
+    /**
+     * 确认订单
+     * @return
+     */
+    @GetMapping
+    public String confirmOrder() {
+
+        return "confirm_order";
+    }
+
+    @PostMapping(value = "/aliPay", produces = "text/html; charset=UTF-8")
+    public String aliPay(String orderId, HttpServletRequest request, HttpServletRequest response) throws AlipayApiException {
+        // 订单信息
+
+        // 产品信息
+
+        //获得初始化的AlipayClient
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.gatewayUrl, AlipayConfig.app_id, AlipayConfig.merchant_private_key, "json", AlipayConfig.charset, AlipayConfig.alipay_public_key, AlipayConfig.sign_type);
+
+        //设置请求参数
+        AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();
+        alipayRequest.setReturnUrl(AlipayConfig.return_url);
+        alipayRequest.setNotifyUrl(AlipayConfig.notify_url);
+
+        //商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = orderId;
+        //付款金额，必填
+        String total_amount = "1";
+        //订单名称，必填
+        String subject = "iphone";
+        //商品描述，可空
+        String body = "用户订购商品个数：1";
+
+        // 该笔订单允许的最晚付款时间，逾期将关闭交易。取值范围：1m～15d。m-分钟，h-小时，d-天，1c-当天（1c-当天的情况下，无论交易何时创建，都在0点关闭）。 该参数数值不接受小数点， 如 1.5h，可转换为 90m。
+        String timeout_express = "30m";
+
+        alipayRequest.setBizContent("{\"out_trade_no\":\""+ out_trade_no +"\","
+                + "\"total_amount\":\""+ total_amount +"\","
+                + "\"subject\":\""+ subject +"\","
+                + "\"body\":\""+ body +"\","
+                + "\"timeout_express\":\""+ timeout_express +"\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+        //请求
+        String result = alipayClient.pageExecute(alipayRequest).getBody();
+
+        return result;
+    }
+
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = new HashMap<>();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        log.info("支付宝回调，sign:{},trade_status:{},参数:{}", params.get("sign"), params.get("trade_status"), params.toString());
+
+        //非常重要，验证回调的正确性，是不是支付宝发的，并且还要避免重复通知
+
+        params.remove("sign");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, AlipayConfig.alipay_public_key, "UTF-8", AlipayConfig.sign_type);
+            if (!alipayRSACheckedV2) {
+                //return ServerResponse.createByErrorMessage("非法请求,验证不通过,在请求我就报警了！");
+            }
+
+        } catch (AlipayApiException e) {
+            log.error("支付宝验证回调异常...", e);
+        }
+        //验证各种数据
+        // 验证订单合法性
+        // 订单状态
+        // 生成支付信息
+        //ServerResponse serverResponse = iOrderService.alipayCallback(params);
+//        if (serverResponse.isSuccess()) {
+//            return Const.AlipayCallback.RESPONSE_SUCCESS;
+//        }
+
+        return null;
+
     }
 }
